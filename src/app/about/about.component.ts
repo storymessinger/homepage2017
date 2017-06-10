a628b59bb62d6b1ee786327fb9691488824a1b96
Jun <Jun@Junui-MacBook-Pro.local>
2017-06-10 18:05:59 +0900

    add

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import 'gsap';
-declare var TweenLite, Power2:any;
+declare var TweenLite, TweenMax, Power2, Linear, TimelineMax, ScrollMagic:any;
 
 @Component({
   selector: 'app-about',
@@ -13,7 +13,30 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    TweenLite.to('.introTitle', 1.4, {opacity: 1, ease: Power2.easeIn});
+    TweenLite.to('.intro', 1.4, {opacity: 1, ease: Power2.easeIn});
+
+    this.parallaxTest();
+  }
+
+  parallaxTest() {
+    const controller = new ScrollMagic.Controller();
+    const tween = new TimelineMax()
+      .add([
+        TweenMax.fromTo("#parallaxContainer .parallax01", 1, 
+          {top:500, ease: Linear.easeNone},
+          {top:0, ease: Linear.easeNone}),
+				TweenMax.to("#parallaxContainer .layer2", 1, {backgroundPosition: "0 -500%", ease: Linear.easeNone}),
+				TweenMax.to("#parallaxContainer .layer3", 1, {backgroundPosition: "0 -225%", ease: Linear.easeNone})
+      ]);
+
+    const scene = new ScrollMagic.Scene({
+      triggerElement: "#parallaxContainer",
+      duration: 2000,
+      offset: 400, 
+    })
+      .setTween(tween)
+      .setPin("#parallaxContainer")
+      .addTo(controller);
   }
 
 }
